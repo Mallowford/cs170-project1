e75622ca446a58506f543135cce134e1d3aefbcb
mallowford <mallowford@gmail.com>
2026-02-11 15:59:45 -0800

    Added predefined depths from dropbox

--- main.cpp
+++ main.cpp
@@ -102,10 +102,16 @@ int main() {
 
     std::vector<std::vector<std::vector<int>>> predefined = {
         {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}}, // Depth 0
-        {{1, 2, 3}, {4, 5, 6}, {0, 7, 8}}
-    } 
+        {{1, 2, 3}, {4, 5, 6}, {0, 7, 8}}, // Depth 2
+        {{1, 2, 3}, {5, 0, 6}, {4, 7, 8}}, // Depth 4
+        {{1, 3, 6}, {5, 0, 2}, {4, 7, 8}}, // Depth 8
+        {{1, 3, 6}, {5, 0, 7}, {4, 7, 8}}, // Depth 12
+        {{1, 6, 7}, {5, 0, 3}, {4, 8, 2}}, // Depth 16
+        {{7, 1, 2}, {4, 8, 5}, {6, 3, 0}}, // Depth 20
+        {{0, 7, 2}, {4, 6, 1}, {3, 5, 8}} // Depth 24
+    }; 
 
-    problem.initial_state = {{7, 0, 2}, {8, 5, 3}, {6, 4, 1}};
+    problem.initial_state = predefined.at(0);
     problem.goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
 
     Node* solution = general_search(problem, A_Star_Manhattan);
